0e3d684c35eb81db8457698db50b56a3876882f0
maniksetia <manik.setia001@gmail.com>
2022-01-06 23:48:08 +0530

    Added RemoveDuplicatesFromSortedArray

---
+++ src/Array/RemoveDuplicatesFromSortedArray.java
@@ -0,0 +1,34 @@
+package Array;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Iterator;
+
+public class RemoveDuplicatesFromSortedArray {
+
+    public static void removeDuplicates(int[] nums) {
+//        HashSet<Integer> hashSet=new HashSet<>();
+//        for(int ele: nums){
+//            hashSet.add(ele);
+//        }
+//        int i=0;
+//        for(int ele: hashSet){
+//            nums[i++]=ele;
+//        }
+        int i=0;
+        for(int j=1; j<nums.length; j++){
+            if(nums[i]!=nums[j]){
+                i++;
+                nums[i]=nums[j];
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] ar={-3,-1,0,0,0,3,3};
+        System.out.println(Arrays.toString(ar));
+        removeDuplicates(ar);
+        System.out.println(Arrays.toString(ar));
+    }
+}
