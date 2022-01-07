963f9b9253c3c223317fa771f6415751b75b86f0
maniksetia <manik.setia001@gmail.com>
2022-01-07 22:59:37 +0530

    Added Second Approach
    Signed-off-by: maniksetia <manik.setia001@gmail.com>

--- src/Array/HeightChecker.java
+++ src/Array/HeightChecker.java
@@ -9,23 +9,44 @@ package Array;
 import java.util.Arrays;
 
 public class HeightChecker {
-    public static int heightChecker(int[] ar){
-        int count=0;
+    public static int heightChecker(int[] ar) {
+        int count = 0;
 
         //Approach1: Use an exact cloned helper array, we'll sort it and then compare the indices with the original array
         //T.C O(nlogn) S.C O(n)
-        int[] helperAr=new int[ar.length];
-        System.arraycopy(ar, 0, helperAr, 0, ar.length);
-        Arrays.sort(helperAr);
+//        int[] helperAr=new int[ar.length];
+//        System.arraycopy(ar, 0, helperAr, 0, ar.length);
+//        Arrays.sort(helperAr);
+//
+//        //comparing starts
+//        for(int i=0; i<ar.length; i++){
+//            if (ar[i] != helperAr[i]) count++;
+//        }
 
-        //comparing starts
+//        return count;
+
+        //Approach2: Make a helper array in which store the occurrence of elements of the initial array.
+        int[] frequency=new int[101];
         for(int i=0; i<ar.length; i++){
-            if (ar[i] != helperAr[i]) count++;
+            frequency[ar[i]]++;
+        }
+
+        int givenArrayIndex=0;
+        for(int i=0; i<frequency.length; i++){
+            if(givenArrayIndex<ar.length){
+                while(frequency[i]!=0){
+                    int pickedElement=i;
+                    frequency[i]--;//we've used that element. Now compare it with the original array.
+                    if(pickedElement!=ar[givenArrayIndex]) count++;
+                    givenArrayIndex++;//shift to next element in the given array.
+                }
+            }
         }
         return count;
     }
     public static void main(String[] args) {
-        int[] ar={1,1,4,2,1,3};
+        int[] ar={2,1,2,1,1,2,2,1};
+//        int[] ar={5,1,2,3,4};
         int numberOfIndices=heightChecker(ar);
         System.out.println("Number of indices: "+numberOfIndices);
     }
