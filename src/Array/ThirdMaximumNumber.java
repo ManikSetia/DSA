860c5a64de4aea0358055e2234e838317a720e63
maniksetia <manik.setia001@gmail.com>
2022-01-08 02:41:30 +0530

    Added ThirdMaximumNumber
    Signed-off-by: maniksetia <manik.setia001@gmail.com>

--- src/Array/ThirdMaximumNumber.java
+++ src/Array/ThirdMaximumNumber.java
@@ -1,26 +1,46 @@
+//Given an integer array nums, return the third distinct maximum number in this array. If the third maximum does not exist, return the maximum number.
+
 package Array;
 
-import java.util.Arrays;
-
 public class ThirdMaximumNumber {
-//    private static int thirdMax(int[] ar){
-//        int firstMax=Integer.MIN_VALUE;
-//        for(int i=0; i<ar.length; i++){
-//            if(firstMax<ar[i]) firstMax=ar[i];
-//        }
-//        int secondMax=Integer.MIN_VALUE;
-//        for(int i=0; i<ar.length; i++){
-//            if(secondMax<firstMax && secondMax>ar[i]) secondMax=ar[i];
-//        }
-//        return secondMax;
-//    }
+    //T.C. O(n)
+    private static int thirdMax(int[] ar){
+        //using Integer class so that we can use null initially.
+        Integer firstMax=null;
+        Integer secondMax=null;
+        Integer thirdMax=null;
+
+        for(Integer ele: ar){
+
+            if(ele.equals(firstMax) || ele.equals(secondMax) || ele.equals(thirdMax)) continue;
+
+            if(firstMax==null || ele>firstMax){
+                //if value of firstMax is getting updated, current value of firstMax will become secondMax and current value of secondMax will become thirdMax
+                thirdMax=secondMax;
+                secondMax=firstMax;
+                firstMax=ele;
+            }
+            else if(secondMax==null || ele>secondMax){
+                //same above reason
+                thirdMax=secondMax;
+                secondMax=ele;
+            }
+            else if(thirdMax==null || ele>thirdMax) thirdMax=ele;
+        }
+
+        //if there is no thirdMax in the array.
+        if(thirdMax==null) return firstMax;
+
+        return thirdMax;
+    }
+
 
     public static void main(String[] args) {
-        int[] ar={2,4,5,6,7};
+//        int[] ar={2,4,5,6,7};
 //        int[] ar={1,2};
-//        int[] ar={2,2,3,1};
-//        int thirdMaximum=thirdMax(ar);
-//        System.out.println("Third maximum number: "+thirdMaximum);
-
+//        int[] ar={-2147483648,1,1};//1
+        int[] ar={1,2,-2147483648};//-2147483648
+        int thirdMaximum=thirdMax(ar);
+        System.out.println("Third maximum number: "+thirdMaximum);
     }
 }
