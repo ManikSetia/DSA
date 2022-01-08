fc021e466f970e3a1f13a1a0d94c2109430a2819
maniksetia <manik.setia001@gmail.com>
2022-01-09 02:31:43 +0530

    Added SwapAlternate
    Signed-off-by: maniksetia <manik.setia001@gmail.com>

---
+++ src/Array/SwapAlternate.java
@@ -0,0 +1,37 @@
+//ip = {1,2,3,4,5,6}
+//op = {2,1,4,3,6,5}
+
+package Array;
+
+import java.util.Arrays;
+
+public class SwapAlternate {
+    private static void swap(int[] ar, int i, int j){
+        int temp=ar[i];
+        ar[i]=ar[j];
+        ar[j]=temp;
+    }
+    private static void swapAlternateElements(int[] ar){
+//        if((ar.length & 1) == 1){
+//            for(int i=0; i<ar.length-1; i=i+2){
+//                swap(ar, i, i+1);
+//            }
+//        }
+//        else{
+//            for(int i=0; i<ar.length; i=i+2){
+//                swap(ar, i, i+1);
+//            }
+//        }
+
+        //Better
+        for(int i=0; i<ar.length; i=i+2){
+            if(i+1 < ar.length) swap(ar, i, i+1);
+        }
+    }
+    public static void main(String[] args) {
+        int[] ar={1,2,3,4,5,6};
+        System.out.println("Before swapping: "+Arrays.toString(ar));
+        swapAlternateElements(ar);
+        System.out.println("After swapping: "+Arrays.toString(ar));
+    }
+}
