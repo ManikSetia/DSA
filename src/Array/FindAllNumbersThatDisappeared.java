b005c926d345eb902aafbee09c3793afb017b5f5
maniksetia <manik.setia001@gmail.com>
2022-01-08 13:27:30 +0530

    Added Brute Force and Better Approaches
    Signed-off-by: maniksetia <manik.setia001@gmail.com>

--- src/Array/FindAllNumbersThatDisappeared.java
+++ src/Array/FindAllNumbersThatDisappeared.java
@@ -9,23 +9,31 @@ public class FindAllNumbersThatDisappeared {
     private static List<Integer> findDisappearedNumbers(int[] ar){
         List<Integer> result=new ArrayList<>();
 
-        //Approach1: Use a helper array to find which numbers are missing.
-        //T.C O(n) S.C O(n)
-        int[] helperAr=new int[ar.length+1];//ar.length+1 is used because we want elements from 1 to size of array
-        for(int i=0; i<ar.length; i++){
-            helperAr[ar[i]]++;
+        //Approach1: Use to for loops to find the missing numbers.
+        //Hint: Use the given constraint; [1,n]
+        for(int element=1; element<=ar.length; element++){
+            int j=0;
+            while(j<ar.length && element!=ar[j]) j++;
+            if(j==ar.length) result.add(element);
         }
 
-        //now helperAr is ready, we just left with iteration in this array to find the missing elements in the given array
-        for(int i=1; i<helperAr.length; i++){//index started from 0 as we want elements from 1 to size of array
-            if(helperAr[i]==0) result.add(i);
-        }
+        //Approach2: Use a helper array to find which numbers are missing.
+        //T.C O(n) S.C O(n)
+//        int[] helperAr=new int[ar.length+1];//ar.length+1 is used because we want elements from 1 to size of array
+//        for(int i=0; i<ar.length; i++){
+//            helperAr[ar[i]]++;
+//        }
+//
+//        //now helperAr is ready, we just left with iteration in this array to find the missing elements in the given array
+//        for(int i=1; i<helperAr.length; i++){//index started from 0 as we want elements from 1 to size of array
+//            if(helperAr[i]==0) result.add(i);
+//        }
 
         return result;
     }
     public static void main(String[] args) {
-//        int[] ar={4,3,2,7,8,2,3,1};
-        int[] ar={1,1};
+        int[] ar={4,3,2,7,8,2,3,1};
+//        int[] ar={1,1};
         List<Integer> findNumbers=findDisappearedNumbers(ar);
         System.out.println("Numbers that disappeared: "+findNumbers);
     }
