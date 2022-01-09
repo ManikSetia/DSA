019673d42d8507c425bc852361615abb7eedc385
maniksetia <manik.setia001@gmail.com>
2022-01-09 16:24:32 +0530

    Added problem description
    Signed-off-by: maniksetia <manik.setia001@gmail.com>

--- src/Array/Level2/FindTheDuplicateNumber.java
+++ src/Array/Level2/FindTheDuplicateNumber.java
@@ -1,3 +1,6 @@
+//Given an array of integers nums containing n + 1 integers where each integer is in the range [1, n] inclusive.
+//There is only one repeated number in nums, return this repeated number.
+
 package Array.Level2;
 
 import java.util.Arrays;
