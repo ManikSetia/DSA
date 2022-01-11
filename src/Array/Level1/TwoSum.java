aeeeb40c61a948ad79cc3bf222794bec32026ddc
maniksetia <manik.setia001@gmail.com>
2022-01-11 20:59:02 +0530

    Added Optimal Approach
    Signed-off-by: maniksetia <manik.setia001@gmail.com>

--- src/Array/Level1/TwoSum.java
+++ src/Array/Level1/TwoSum.java
@@ -4,23 +4,31 @@
 
 package Array.Level1;
 
-import java.util.Arrays;
+import java.util.*;
 
 public class TwoSum {
     private static int[] twoSum(int[] ar, int target){
-        int[] indices=new int[2];
 
         //Approach1: Use two loops.
         //T.C (n2) S.C O(1)
+//        for(int i=0; i<ar.length; i++){
+//            for(int j=i+1; j<ar.length; j++){
+//                int sum=ar[i]+ar[j];
+//                if(sum==target) return new int[] {i, j};
+//            }
+//        }
+
+        //Approach2: Sorting and two pointer.
+        //T.C O(n) S.C O(n)
+        //using hashMap to store the elements with their indices.
+        Map<Integer, Integer> map=new HashMap<>();
         for(int i=0; i<ar.length; i++){
-            for(int j=i+1; j<ar.length; j++){
-                if(ar[i]+ar[j]==target) {
-                    indices[0]=i;
-                    indices[1]=j;
-                }
-            }
+            int secondNumber=target-ar[i];
+            if(map.containsKey(secondNumber)) return new int[] {map.get(secondNumber), i};
+            map.put(ar[i], i);
         }
-        return indices;
+
+        return ar;
     }
     public static void main(String[] args) {
 //        int[] ar={2,7,11,15};
